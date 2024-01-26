e50c9b9ec11abf83485213ba4a8a7db400566688
Oleksandra Mala <oleksandra.s.mala@gmail.com>
2024-01-26 01:50:28 +0100

    Routing

--- Routing.php
+++ Routing.php
@@ -35,4 +35,6 @@ class Routing {
 
         $object->$action();
     }
+
+
 }
--- index.php
+++ index.php
@@ -8,9 +8,18 @@ $path = parse_url( $path, PHP_URL_PATH);
 Routing::get('', 'DefaultController');
 Routing::get('categories', 'CategoryController');
 Routing::get('reviews', 'ReviewController');
+Routing::get('reviewDetails', 'ReviewController');
 Routing::post('login', 'SecurityController');
 Routing::post('logout', 'SecurityController');
 Routing::post('registration', 'SecurityController');
+
+
+Routing::post('add', 'ReviewController');
+Routing::post('delete', 'ReviewController');
+Routing::get('update', 'ReviewController');
+Routing::post('update', 'ReviewController');
+
+
 
 
 
